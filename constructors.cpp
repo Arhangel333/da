92e03ff53636a6e5a7db65925ffe27fa97fa4814
Arhangel333 <71559868+Arhangel333@users.noreply.github.com>
2020-12-05 00:42:25 +0300

    Add files via upload

---
+++ constructors.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include "foo.cpp"
+using namespace std;
+//const int  PASTING_BALANCING = 0;
+#define  PASTING_BALANCING
+#define  DELETING_BALANCE
+const int CONSTRUCTORS = 0, TREE_CONSTRUCTORS = 0, SHOW_TREE_IN_BALANCE = 0;
+//вставка вроде доделана
+//теперь удОление
+//исправлял левый и правый повороты
