4cb1555ef7f1ee9e01aa416798ebdece017e5047
Arhangel333 <71559868+Arhangel333@users.noreply.github.com>
2025-03-22 19:42:52 +0300

    Update main.cpp
    теперь пустые строки из входного файла удаляются

--- lab1.25/main.cpp
+++ lab1.25/main.cpp
@@ -51,23 +51,60 @@ void radixSort(std::vector < std::pair < std::string, std::string>> &data)
 }
 
 int main()
-{
-    std::vector < std::pair < std::string, std::string>> data;
-    std::string key, value;
-    printf("One");
-    while((std::getline(std::cin, key)) || (std::getline(std::cin, value)))
-    {
-        
-        data.emplace_back(key, value);
-        std::cout << "Добавлено: " << data.back().first << " -> " << data.back().second << '\n';
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+char getChar(const std::string &key, int pos) {
+    if (pos < key.length()) {
+        return key[pos];
     }
-    // Сортировка
+    return 0;
+}
+
+void radixSort(std::vector<std::pair<std::string, std::string>> &data) {
+    const int maxLen = 9;
+    for (int pos = maxLen - 1; pos >= 0; --pos) {
+        std::vector<std::pair<std::string, std::string>> output(data.size());
+        std::vector<int> count(256, 0);
+
+        for (const auto &pair : data) {
+            char ch = getChar(pair.first, pos);
+            count[ch]++;
+        }
+
+        for (int i = 1; i < 256; ++i) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = data.size() - 1; i >= 0; --i) {
+            char ch = getChar(data[i].first, pos);
+            output[count[ch] - 1] = data[i];
+            count[ch]--;
+        }
+
+        data = output;
+    }
+}
+
+int main() {
+    std::vector<std::pair<std::string, std::string>> data;
+    std::string line;
+
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) continue;
+        size_t tabPos = line.find('\t');
+        if (tabPos == std::string::npos) continue;
+        std::string key = line.substr(0, tabPos);
+        std::string value = line.substr(tabPos + 1);
+        data.emplace_back(key, value);
+    }
+
     radixSort(data);
 
-    // Вывод отсортированных данных
-    for (const auto &pair : data)
-    {
-        std::cout<<pair.first << '\t' << pair.second << '\n';
+    for (const auto &pair : data) {
+        std::cout << pair.first << '\t' << pair.second << '\n';
     }
 
     return 0;
